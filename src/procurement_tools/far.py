1a0af16094602bc3567b9a16cec42a2ae3768508
V. David Zvenyach <dave@zvenyach.com>
2023-11-21 10:22:13 -0600

    :sparkles: Working FAR section getter

--- src/procurement_tools/far.py
+++ src/procurement_tools/far.py
@@ -1,11 +1,11 @@
 from .models.far_clause import Clause
+from bs4 import BeautifulSoup
 from io import StringIO
 import requests
 import textwrap
 import unicodedata
-from lxml import html
-from lxml import etree
-from lxml.html.clean import clean_html
+from lxml import etree, html
+from lxml.html.soupparser import fromstring
 
 """
 
@@ -20,10 +20,20 @@ class FAR:
         res = requests.get(URL)
         if res.status_code == 200:
             parser = etree.HTMLParser(remove_blank_text=True)
-            tree = html.parse(StringIO(textwrap.dedent(res.text)), parser)
-            body = "".join(tree.xpath("//div[contains(@class,'body')]//text()")).strip()
+            tree = html.parse(
+                StringIO(textwrap.shorten(res.text, len(res.text))), parser
+            )
+            body = tree.xpath("//div[contains(@class,'body')]")[0]
+            soup = BeautifulSoup(etree.tostring(body), "html.parser")
+            text = "\n".join(
+                [
+                    para.get_text(strip=True, separator=" ")
+                    for para in soup.find_all("p")
+                    if para.get_text(strip=True)
+                ]
+            )
             return Clause(
                 title=tree.xpath("//title")[0].text,
                 number=tree.xpath("//h1")[0].cssselect("span")[0].text,
-                body=unicodedata.normalize("NFKD", body),
+                body=text,
             )
