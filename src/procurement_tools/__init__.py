94f954f9d62abb3d1204ea8dccc28f17929e958c
V. David Zvenyach <dave@zvenyach.com>
2023-11-30 19:45:50 -0600

    :sparkles: Added Periodic Table of Acquisition Innovations

--- src/procurement_tools/__init__.py
+++ src/procurement_tools/__init__.py
@@ -1,6 +1,7 @@
 from .usaspending import USASpending
 from .uei import UEI
 from .far import FAR
+from .periodic_table import PeriodicTable
 
 
-__all__ = ["USASpending", "UEI", "FAR"]
+__all__ = ["USASpending", "UEI", "FAR", "PeriodicTable"]
---
+++ src/procurement_tools/periodic_table.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import httpx
+import json
+import os
+import random
+import re
+
+with open(
+    os.path.join(
+        os.path.dirname(__file__),
+        "data/periodic_table.json",
+    ),
+    "r",
+) as fp:
+    INNOVATIONS = json.load(fp)
+
+
+class PeriodicTable:
+    innovations = INNOVATIONS
+
+    @classmethod
+    def get_random_innovation(cls):
+        idx = random.randint(0, len(cls.innovations) - 1)
+        return cls.innovations[idx]
+
+    @staticmethod
+    def create_data() -> dict:
+        """Create a dict with the FAI Periodic Table of Acquisition Innovation elements"""
+        req = httpx.get("https://www.fai.gov/periodic-table")
+        soup = BeautifulSoup(req, "html.parser")
+
+        results = []
+
+        columns = soup.find_all(class_="pt-card")
+        for column in columns:
+            res = re.split(r"\n\s+\n", column.find(class_="cardTitle").get_text())
+            column_title = res[0]
+            column_subtitle = res[1]
+            tiles = column.find_all(class_="tile")
+            for tile in tiles:
+                title = tile.find(class_="views-field-title").get_text().strip()
+                sections = [
+                    elem.get_text() for elem in tile.find_all(class_="popup-subtitle")
+                ]
+                description = sections[0]
+                problems_solved = sections[1]
+                benefits = sections[2]
+                results.append(
+                    dict(
+                        column_title=column_title,
+                        title=title,
+                        description=description,
+                        problems_solved=problems_solved,
+                        benefits=benefits,
+                    )
+                )
+        return results
